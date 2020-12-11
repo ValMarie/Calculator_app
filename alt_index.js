4584b24ec4d2734fbc3aa6cecf027d21be319000
ValMarie <valanthonio-marie@yahoo.com>
2020-12-11 19:40:06 +0100

    updated calculator

---
+++ alt_index.js
@@ -0,0 +1,92 @@
+
+    // displayValue: 0,
+    // firstInputValue: Number,
+    // secondInputValue: Number,
+    // _operator: false,
+    // get operator() {
+    //     return this._operator;
+    // },
+    // set operator(value) {
+    //     this._operator = value;
+    
+
+// keysClicked =()=>
+//  document.addEventListener("click",(event)=>{
+//     for (let index = 0; index < buttons.length; index++) {
+//         let key = buttons[index].value;
+//         key == event.target;
+//         if (event.target.matches("button")) {
+//             return (key);
+//         }
+//     }
+// });
+
+
+// let display = document.getElementById('calculator_display');
+//     display.innerHTML = calculator.displayValue;
+
+ 
+// keysClicked == numbers? console.log ("nomber clicked"): console.log ("none");
+
+    // let num1 = calculator.firstInputValue;
+    // let num2 = calculator.secondInputValue;
+    // // num1 = numbers.display.innerHTML
+    // num2 = numbers.display.innerHTML
+
+    
+
+
+    // number = document.getElementsByClassName("number_key")
+    // id.innerHTML = calculator.firstInputValue;
+    // calculator.firstInputValue = id.innerHTML
+    
+
+
+ 
+// let number1 = document.getElementsByClassName('number_key');
+//     console.log(number1.innerHTML);
+//     number1.display.innerHTML = calculator.firstInputValue;
+//     console.log (number1.display.innerHTML);
+    
+// let number2 = document.getElementsByClassName('number_key');
+//     number2.innerHTML = calculator.secondInputValue;
+//     console.log (number2.innerHTML);
+
+// let add = document.getElementById('operator_add');
+    
+
+// let keys = document.getElementById('calculator_keys');
+//     //  keys.innerHTML = display.displayValue;
+
+//         console.log ('number keys');
+    
+        
+        
+    
+        
+        // ||
+        //     key_action.value == subtract ||
+        //     key_action.value == multiply ||
+        //     key_action.value == divide) {
+        //         console.log ('operator keys');
+        // }
+    
+        // if (key_action == decimal){
+        //     console.log ('decimal');
+        // }
+    
+        // if (key_action == clear) {
+        //         console.log ('reset')
+        // };
+    
+        // if (key_action == equals){
+        //         console.log (result)
+        // };
+          
+        // }
+//  });
+
+      
+
+// if ({
+//     reset = calculator.displayValue
--- index.js
+++ index.js
@@ -1,77 +1,133 @@
-const calculator = {
-    displayValue: 0,
-    firstInputValue: Number,
-    secondInputValue: Number,
-    _operator: false,
-    get operator() {
-        return this._operator;
-    },
-    set operator(value) {
-        this._operator = value;
-    },
+let buttons = document.querySelectorAll("button");
+let numberKey = document.querySelectorAll(".number_key");
+let operatorKey = document.querySelectorAll(".operator_key");
+let deleteKey = document.querySelector("#delete");
+let resetKey = document.querySelector("#reset");
+let equalsKey = document.querySelector("#calculate");
+let functionKey = document.querySelectorAll("#function_keys");
+let previousDisplayedNumber = document.querySelector("#previousDisplay");
+let currentDisplayedNumber = document.querySelector("#currentDisplay");
+
+class Calculator  {
+    constructor (previousDisplayedNumber, currentDisplayedNumber){
+        this.previousDisplayedNumber = previousDisplayedNumber;
+        this.currentDisplayedNumber = currentDisplayedNumber;
+        this.resetDisplay()
+        this.clearAllDisplay();
+    };
+
+    clearAllDisplay(){
+    // clears all display on the screen
+        this.previousNumber = "";
+        this.currentNumber = "";
+        this.operator = "";
+    }
+
+    resetDisplay(){
+        // resets display on the screen
+            this.previousNumber = "";
+            this.currentNumber = "0"; 
+            this.operator = "";
+            this.previousDisplayedNumber.innerText = "";
+    }
+
+    appendNumber(number){
+    // adds number input on display when button is clicked
+        if (number === "." && this.currentNumber.includes(".")) return;
+        if (this.currentNumber === "0" && !this.currentNumber.includes("."))
+            return this.currentNumber =  this.currentNumber + number.toString();
+        // // if (this.calculate >= "0") return number
+        this.currentNumber = this.currentNumber + number.toString();
+    }
+
+    deleteNumber(){
+    // deletes numbers inputted from behind        
+        this.currentNumber = this.currentNumber.toString().slice(0, -1)
+        if (this.currentNumber === "") return  this.resetDisplay()
+    }
+
+    chooseOperator(operator){
+    // selects the operator function without appending to display
+    if (this.previousNumber !== "") {
+        this.calculate()
+        }
+        this.operator = operator;
+        this.previousNumber = this.currentNumber;
+        this.currentNumber = "";
+    }
+
+    calculate(){
+    // compute the input and displays
+        let calculations
+        let pre_value = parseFloat(this.previousNumber);
+        let curr_value = parseFloat(this.currentNumber);
+        if (isNaN(pre_value) || isNaN(curr_value)) return
+
+        switch (this.operator) {
+            case "+":
+                calculations = pre_value + curr_value;
+                break;
+            case "-":
+                calculations = pre_value - curr_value;
+                break;
+            case "x":
+                calculations = pre_value * curr_value;
+                break;
+            case "/":
+                calculations = pre_value / curr_value;
+                break;
+
+            default:
+                return;
+                }
+        
+        this.operator = "";
+        this.previousNumber = " ";
+        this.currentNumber = calculations;
+    }
+
+    upDateDisplay(){
+    // updates every action or function displayed
+        this.currentDisplayedNumber.innerText = this.currentNumber;
+        if(this.operator != ""){
+            this.previousDisplayedNumber.innerText = this.previousNumber + this.operator + this.currentNumber.toLocaleString("en");
+        }
+        
+    }
 };
 
+let myCalculator = new Calculator (previousDisplayedNumber, currentDisplayedNumber)
 
-var display = document.getElementById('calculator_display');
-    display.innerHTML = calculator.displayValue;
-    console.log (display.innerHTML);
-    
-var number1 = document.getElementsByClassName('number_key');
-    number1.display.innerHTML = calculator.firstInputValue;
-    console.log (number1.value);
-    
-var number2 = document.getElementsByClassName('number_key');
-    number2.innerHTML = calculator.secondInputValue;
-    console.log (number2.innerHTML);
+numberKey.forEach(button => {
+   button.addEventListener("click", () => {
+       myCalculator.appendNumber(button.innerText)
+       myCalculator.upDateDisplay();
+   })
+});
 
-var add = document.getElementById('operator_add');
-    
+operatorKey.forEach(button => {
+    button.addEventListener("click", () => {
+        myCalculator.chooseOperator(button.innerText)
+        myCalculator.upDateDisplay();
+    })
+ });
 
-var keys = document.getElementById('calculator_keys');
-    //  keys.innerHTML = display.displayValue;
+ equalsKey.addEventListener("click", () => {
+     myCalculator.calculate();
+     myCalculator.upDateDisplay();
+ });
 
-        console.log ('number keys');
-    
-        
-        
-    
-        
-        // ||
-        //     key_action.value == subtract ||
-        //     key_action.value == multiply ||
-        //     key_action.value == divide) {
-        //         console.log ('operator keys');
-        // }
-    
-        // if (key_action == decimal){
-        //     console.log ('decimal');
-        // }
-    
-        // if (key_action == clear) {
-        //         console.log ('reset')
-        // };
-    
-        // if (key_action == equals){
-        //         console.log (result)
-        // };
-          
-        // }
-//  });
+ resetKey.addEventListener("click", () => {
+    myCalculator.resetDisplay();
+    myCalculator.upDateDisplay();
+});
 
-      
+resetKey.addEventListener("dblclick", () => {
+    myCalculator.clearAllDisplay();
+    myCalculator.upDateDisplay();
+});
 
-// if ({
-//     reset = calculator.displayValue
-// })
-   
-        
-
-
-   
-
-
-
-
-// var keys = document.addEventListener('click', e=>{
-//     keys = e.target;
-//       if(e.target.matches('button')){}
+deleteKey.addEventListener("click", () => {
+    myCalculator.deleteNumber();
+    myCalculator.upDateDisplay();
+});
